ca6e04e53b68e819ba1626ac5e85ebf991a001c8
ramya <ramya.m@spritle.com>
2020-04-06 23:05:35 +0530

    Showed list of data

--- src/app/crib-list/crib-list.component.ts
+++ src/app/crib-list/crib-list.component.ts
@@ -1,15 +1,14 @@
 import { Component, OnInit } from '@angular/core';
+import { cribs } from './../data/cribs';
 
 @Component({
   selector: 'app-crib-list',
   templateUrl: './crib-list.component.html',
-  styleUrls: ['./crib-list.component.css']
+  styleUrls: ['./crib-list.component.css'],
 })
 export class CribListComponent implements OnInit {
+  cribs: Array<any> = cribs;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
