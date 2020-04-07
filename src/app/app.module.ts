900b0a4b0983775399d035d46cbfd7ac82d61a12
ramya <ramya.m@spritle.com>
2020-04-07 20:04:27 +0530

    Reuse the datalist component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CribListComponent } from './crib-list/crib-list.component';
 import { ComponentSampleComponent } from './component-sample/component-sample.component';
+import { CribCardComponent } from './crib-card/crib-card.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     CribListComponent,
-    ComponentSampleComponent
+    ComponentSampleComponent,
+    CribCardComponent
   ],
   imports: [
     BrowserModule,
